9ba1ffa56658e6037174b562bcf0228ee46e7823
Miyajima Takaki <takaki.mz@gmail.com>
2020-06-02 13:34:40 +0900

    feat: background.jsで位置情報を取得

--- src/public/js/background.js
+++ src/public/js/background.js
@@ -1,17 +1,18 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color: '#3aa757' }, () => {
-    console.log('The color is green.');
+const getGps = () => {
+  return new Promise((resolve, reject) => {
+    // 一回限りの位置取得
+    navigator.geolocation.getCurrentPosition(resolve);
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-         pageUrl: { hostEquals: 'developer.chrome.com' },
-        })
-      ],
-        actions: [
-          new chrome.declarativeContent.ShowPageAction()
-        ]
-    }]);
-  });
-});
+};
+
+// 位置を取得
+const setPosition = async () => {
+  const latitude = await config.get('latitude');
+  const longitude = await config.get('longitude');
+  if (!latitude || !longitude) {
+    window.position = await getGps();
+  }
+}
+
+// 1分毎に位置を取得し直す
+setInterval(setPosition, 60*1000);
