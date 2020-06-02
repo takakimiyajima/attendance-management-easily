df7ce342c16a1a8a2f6de5845d26811dd8fac9fd
Miyajima Takaki <takaki.mz@gmail.com>
2020-06-02 14:01:17 +0900

    feat: permissionsのstorageに関するjs（config.js）の設定

---
+++ src/public/js/config.js
@@ -0,0 +1,49 @@
+// localStorageの設定
+const config = () => {
+  const set = (key, val) => {
+    return new Promise((resolve, reject) => {
+      console.log('config.set', key, val);
+      const pair = {};
+      pair[key] = val;
+      chrome.storage.sync.set(pair, resolve);
+    });
+  }
+
+  const get = (key) => {
+    return new Promise(function(resolve, reject){
+      console.log('config.get', key);
+      chrome.storage.sync.get([key], function(res){
+          resolve(res[key]);
+      });
+    });
+  };
+
+  const setLocal = (key, val) => {
+    return new Promise(function(resolve, reject){
+      console.log('config.setLocal', key, val);
+      const pair = {};
+      pair[key] = val;
+      chrome.storage.local.set(pair, resolve);
+    });
+  };
+
+  const getLocal = (key) => {
+    return new Promise(function(resolve, reject){
+      // console.log('config.getLocal', key);
+      chrome.storage.local.get([key], function(res){
+          resolve(res[key]);
+      });
+    });
+  };
+
+  const clearLocal = () => {
+    return new Promise(function(resolve, reject){
+      console.log('config.clearLocal');
+      chrome.storage.local.clear(resolve);
+    });
+  }
+
+  return {
+    set, get, setLocal, getLocal, clearLocal
+  };
+}
